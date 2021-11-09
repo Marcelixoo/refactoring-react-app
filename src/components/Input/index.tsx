03c5cd6fc33a338da82a403ffe950bfa177bceda
Marcelo Teixeira dos Santos <marcelo.teixeira@cern.ch>
2021-11-09 18:39:49 +0100

    Make icon an optional prop

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,7 +13,7 @@ import { IconType } from 'react-icons/lib';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: IconType;
+  icon?: IconType;
 }
 
 const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
